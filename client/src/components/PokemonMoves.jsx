8904e1faeaaa86a3200fb35ba6762400254de371
Anne Pruett <annepruett@Anne-Pruetts-MacBook.local>
2017-12-03 18:40:04 -0800

    Fix logic in map function to loop through moves array

--- client/src/components/PokemonMoves.jsx
+++ client/src/components/PokemonMoves.jsx
@@ -4,12 +4,12 @@ import css from '../styles.css';
 const PokemonMoves = (props) => {
 
 	return (
-			<div className={css.stats}>
-        <h3>Attacks</h3>
-        {props.pokemon.map(move =>
-          <li key={move.name}>{move.name}</li>)}
-      </div>  
-		);
+		<div className={css.stats}>
+      <h3>Attacks</h3>
+      {props.pokemon.moves.map(move =>
+        <li key={move.name}>{move.name}</li>)}
+    </div>  
+	);
 
 }
 
